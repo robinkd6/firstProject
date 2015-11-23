d7207f5a0606bc269597746f8cb0c8f16cbaf5c9
robinkd6 <rkdcruz18@gmail.com>
2015-11-23 08:47:08 -0800

    worked on styling of game

---
+++ main.js
@@ -0,0 +1,15 @@
+var counter = 0;
+var imgFound = 0;
+var boxOpened = "";
+var imgOpened = "";
+var source = ""
+var imgSource = [];
+
+function randomImg(maxValue, minValue) {
+	return Math.round(Math.random() * (maxValue - minValue) +
+		minValue);
+}
+
+function shuffleImgs() {
+	var allImgs = $()
+}
